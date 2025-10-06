1ae0925e70be5b0901bf4b4076d1c57ce777e9fa
vegemiiBG <twitchvegemii@gmail.com>
2025-10-06 00:47:26 -0400

    Generation of VF Safe Zone now working

--- location_names/virtua_forest_safe_zone.py
+++ location_names/virtua_forest_safe_zone.py
@@ -1,75 +1,31 @@
 #Virtua Forest Safe Zone - ID 1
-#Gather Point 1
-virtua_forest_safe_zone_gather1_reward1 = "Virtua Forest Safe Zone - Gather 1 - Herb 1"
-virtua_forest_safe_zone_gather1_reward2 = "Virtua Forest Safe Zone - Gather 1 - Herb 2"
-virtua_forest_safe_zone_gather1_reward3 = "Virtua Forest Safe Zone - Gather 1 - Herb 3"
-virtua_forest_safe_zone_gather1_reward4 = "Virtua Forest Safe Zone - Gather 1 - Dogoo Jelly 1"
-virtua_forest_safe_zone_gather1_reward5 = "Virtua Forest Safe Zone - Gather 1 - Dogoo Jelly 2"
+import typing
+from ..LocationData import LocationData
 
-#Gather Point 1 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather1_reward1 = "Virtua Forest Safe Zone - Gather 1 (Change Dungeon) - Dogoo Jelly 1"
-virtua_forest_safe_zone_changedungeon_gather1_reward2 = "Virtua Forest Safe Zone - Gather 1 (Change Dungeon) - Dogoo Jelly 2"
-
-#Gather Point 2
-virtua_forest_safe_zone_gather2_reward1 = "Virtua Forest Safe Zone - Gather 2 - Herb 1"
-virtua_forest_safe_zone_gather2_reward2 = "Virtua Forest Safe Zone - Gather 2 - Herb 2"
-virtua_forest_safe_zone_gather2_reward3 = "Virtua Forest Safe Zone - Gather 2 - Herb 3"
-virtua_forest_safe_zone_gather2_reward4 = "Virtua Forest Safe Zone - Gather 2 - Lizard Scale 1"
-virtua_forest_safe_zone_gather2_reward5 = "Virtua Forest Safe Zone - Gather 2 - Lizard Scale 2"
-
-#Gather Point 2 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather2_reward1 = "Virtua Forest Safe Zone - Gather 2 (Change Dungeon) - Lizard Scale 1"
-virtua_forest_safe_zone_changedungeon_gather2_reward2 = "Virtua Forest Safe Zone - Gather 2 (Change Dungeon) - Lizard Scale 2"
-
-#Gather Point 3
-virtua_forest_safe_zone_gather3_reward1 = "Virtua Forest Safe Zone - Gather 3 - Yellow Petal 1"
-virtua_forest_safe_zone_gather3_reward2 = "Virtua Forest Safe Zone - Gather 3 - Yellow Petal 2"
-virtua_forest_safe_zone_gather3_reward3 = "Virtua Forest Safe Zone - Gather 3 - Yellow Petal 3"
-virtua_forest_safe_zone_gather3_reward4 = "Virtua Forest Safe Zone - Gather 3 - Yellow Petal 4"
-virtua_forest_safe_zone_gather3_reward5 = "Virtua Forest Safe Zone - Gather 3 - Yellow Petal 5"
-
-#Gather Point 3 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather3_reward1 = "Virtua Forest Safe Zone - Gather 3 (Change Dungeon) - Yellow Petal 1"
-virtua_forest_safe_zone_changedungeon_gather3_reward1 = "Virtua Forest Safe Zone - Gather 3 (Change Dungeon) - Yellow Petal 2"
-
-#Gather Point 4
-virtua_forest_safe_zone_gather4_reward1 = "Virtua Forest Safe Zone - Gather 4 - Herb 1" 
-virtua_forest_safe_zone_gather4_reward2 = "Virtua Forest Safe Zone - Gather 4 - Herb 2"
-virtua_forest_safe_zone_gather4_reward3 = "Virtua Forest Safe Zone - Gather 4 - Coin Fragment 1"
-virtua_forest_safe_zone_gather4_reward4 = "Virtua Forest Safe Zone - Gather 4 - Coin Fragment 2"
-
-#Gather Point 4 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather4_reward1 = "Virtua Forest Safe Zone - Gather 4 (Change Dungeon) - Coin Fragment 1"
-virtua_forest_safe_zone_changedungeon_gather4_reward2 = "Virtua Forest Safe Zone - Gather 4 (Change Dungeon) - Coin Fragment 2"
-virtua_forest_safe_zone_changedungeon_gather4_reward3 = "Virtua Forest Safe Zone - Gather 4 (Change Dungeon) - Dogoo Jelly 1"
-virtua_forest_safe_zone_changedungeon_gather4_reward4 = "Virtua Forest Safe Zone - Gather 4 (Change Dungeon) - Dogoo Jelly 2"
-
-#Gather Point 5
-virtua_forest_safe_zone_gather5_reward1 = "Virtua Forest Safe Zone - Gather 5 - Herb 1"
-virtua_forest_safe_zone_gather5_reward2 = "Virtua Forest Safe Zone - Gather 5 - Herb 2"
-
-#Gather Point 5 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather5_reward1 = "Virtua Forest Safe Zone - Gather 5 (Change Dungeon) - Herb 1"
-virtua_forest_safe_zone_changedungeon_gather5_reward2 = "Virtua Forest Safe Zone - Gather 5 (Change Dungeon) - Herb 2"
-virtua_forest_safe_zone_changedungeon_gather5_reward3 = "Virtua Forest Safe Zone - Gather 5 (Change Dungeon) - Lizard Scale 1"
-virtua_forest_safe_zone_changedungeon_gather5_reward4 = "Virtua Forest Safe Zone - Gather 5 (Change Dungeon) - Lizard Scale 2"
-
-#Gather Point 6
-virtua_forest_safe_zone_gather6_reward1 = "Virtua Forest Safe Zone - Gather 6 - Herb 1"
-virtua_forest_safe_zone_gather6_reward2 = "Virtua Forest Safe Zone - Gather 6 - Herb 2"
-
-#Gather Point 6 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather6_reward1 = "Virtua Forest Safe Zone - Gather 6 (Change Dungeon) - Herb 1"
-virtua_forest_safe_zone_changedungeon_gather6_reward2 = "Virtua Forest Safe Zone - Gather 6 (Change Dungeon) - Herb 2"
-virtua_forest_safe_zone_changedungeon_gather6_reward3 = "Virtua Forest Safe Zone - Gather 6 (Change Dungeon) - Yellow Petal 1"
-virtua_forest_safe_zone_changedungeon_gather6_reward4 = "Virtua Forest Safe Zone - Gather 6 (Change Dungeon) - Yellow Petal 2"
-
-#Gather Point 7
-virtua_forest_safe_zone_gather7_reward1 = "Virtua Forest Safe Zone - Gather 7 - Sunflowery Seed 1"
-virtua_forest_safe_zone_gather7_reward2 = "Virtua Forest Safe Zone - Gather 7 - Sunflowery Seed 2"
-
-#Gather Point 7 - Change Dungeon
-virtua_forest_safe_zone_changedungeon_gather7_reward1 = "Virtua Forest Safe Zone - Gather 7 (Change Dungeon) - Sunflowery Seed 1"
-virtua_forest_safe_zone_changedungeon_gather7_reward2 = "Virtua Forest Safe Zone - Gather 7 (Change Dungeon) - Sunflowery Seed 2"
-virtua_forest_safe_zone_changedungeon_gather7_reward3 = "Virtua Forest Safe Zone - Gather 7 (Change Dungeon) - Coin Fragment 1"
-virtua_forest_safe_zone_changedungeon_gather7_reward4 = "Virtua Forest Safe Zone - Gather 7 (Change Dungeon) - Coin Fragment 2"
+VirtuaForestSafeZone: typing.List[LocationData] = (
+LocationData("Virtua Forest Safe Zone - Gather 1 - Herb 1", 1, 0),
+LocationData("Virtua Forest Safe Zone - Gather 1 - Herb 2", 2, 0),
+LocationData("Virtua Forest Safe Zone - Gather 1 - Herb 3", 3, 0),
+LocationData("Virtua Forest Safe Zone - Gather 1 - Dogoo Jelly 1", 4, 0),
+LocationData("Virtua Forest Safe Zone - Gather 1 - Dogoo Jelly 2", 5, 0),
+LocationData("Virtua Forest Safe Zone - Gather 2 - Herb 1", 6, 0),
+LocationData("Virtua Forest Safe Zone - Gather 2 - Herb 2", 7, 0),
+LocationData("Virtua Forest Safe Zone - Gather 2 - Herb 3", 8, 0),
+LocationData("Virtua Forest Safe Zone - Gather 2 - Lizard Scale 1", 9, 0),
+LocationData("Virtua Forest Safe Zone - Gather 2 - Lizard Scale 2", 10, 0),
+LocationData("Virtua Forest Safe Zone - Gather 3 - Yellow Petal 1", 11, 0),
+LocationData("Virtua Forest Safe Zone - Gather 3 - Yellow Petal 2", 12, 0),
+LocationData("Virtua Forest Safe Zone - Gather 3 - Yellow Petal 3", 13, 0),
+LocationData("Virtua Forest Safe Zone - Gather 3 - Yellow Petal 4", 14, 0),
+LocationData("Virtua Forest Safe Zone - Gather 3 - Yellow Petal 5", 15, 0),
+LocationData("Virtua Forest Safe Zone - Gather 4 - Herb 1", 16, 0),
+LocationData("Virtua Forest Safe Zone - Gather 4 - Herb 2", 17, 0),
+LocationData("Virtua Forest Safe Zone - Gather 4 - Coin Fragment 1", 18, 0),
+LocationData("Virtua Forest Safe Zone - Gather 4 - Coin Fragment 2", 19, 0),
+LocationData("Virtua Forest Safe Zone - Gather 5 - Herb 1", 20, 0),
+LocationData("Virtua Forest Safe Zone - Gather 5 - Herb 2", 21, 0),
+LocationData("Virtua Forest Safe Zone - Gather 6 - Herb 1", 22, 0),
+LocationData("Virtua Forest Safe Zone - Gather 6 - Herb 2", 23, 0),
+LocationData("Virtua Forest Safe Zone - Gather 7 - Sunflowery Seed 1", 24, 0),
+LocationData("Virtua Forest Safe Zone - Gather 7 - Sunflowery Seed 2", 25, 0)
+)
